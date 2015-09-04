b0f770e437fad4f4f67b3bc2eb9a0bce38bd6050
Matthew Newell <matthew.r.newell@gmail.com>
2015-09-04 10:07:48 -0400

    Copy Roo hash into new hash.

--- vet_scripts.rb
+++ vet_scripts.rb
@@ -1,21 +1,29 @@
 require 'roo'
 require 'pry'
 
-appointments = Roo::Excelx.new('inputs/appointments.xlsx')
+appointments_hash = Hash.new
 
+appointments = Roo::Excelx.new('inputs/appointments.xlsx')
 appointments_worksheet = appointments.sheet(0)
 
 appointments_worksheet.each(
   name: 'Pet Name',
   date: 'Appointment Date'
   ) do |hash|
-  puts hash.inspect
-  binding.pry
+  
+  hash_key = hash[:name]
+
+  appointments_hash[hash_key] = {
+    name: hash[:name],
+    date: hash[:date]
+  }
 end
 
-# Try these commands in pry. Type 'exit' to continue iteration.
-# hash.keys
-# hash.values
-# hash[:name]
-# hash[:date]
+binding.pry
+
+# Try these commands in pry.
+# 
+# appointments_hash
+# appointments_hash["Rex"]
+# appointments_hash["Rex"][:date]
 
